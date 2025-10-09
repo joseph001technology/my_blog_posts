7f6b093cafe0cbb49f9420f4c2e9d2fc0a96802e
joseph <josenkiarie @gmail.com>
2025-10-09 15:07:02 +0300

    updated tests for new CustomUser

--- accounts/tests.py
+++ accounts/tests.py
@@ -13,7 +13,7 @@ User = get_user_model()
 class RegistrationTestCase(APITestCase):
     def test_user_registration(self):
         data = {
-        "username": "testuser",
+        "user_name": "testuser",
         "email": "testuser@josek.com",
         "password": "PASwword1234",
         "first_name": "Test",
@@ -23,7 +23,7 @@ class RegistrationTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         
         self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, "testuser")
+        self.assertEqual(User.objects.get().user_name, "testuser")
         
         
         
@@ -31,14 +31,14 @@ class UserProfileTestCase(TestCase):
     def setUp(self):
         # Create user
         self.user = User.objects.create_user(
-            username="testuser1",
+            user_name="testuser1",
             email="testuser1@josek.com",
             password="testpassword123",
             first_name="Test",
             
         )
         # Log in user with Django’s test client (session auth)
-        self.client.login(username="testuser1", password="testpassword123")
+        self.client.login(user_name="testuser1", password="testpassword123")
 
     def test_user_profile_create(self):
         response = self.client.post(reverse('userauth:edit'), {
@@ -60,20 +60,20 @@ class UserProfileTestCase(TestCase):
           
 class TestUserModel(TestCase):
   def test_create_user(self):
-      user = User.objects.create_user(username='testuser',password="password", first_name="Test")   
-      self.assertEqual(user.username, 'testuser')
+      user = User.objects.create_user(user_name='testuser',password="password", first_name="Test", email="testuser@example.com")   
+      self.assertEqual(user.user_name, 'testuser')
       self.assertTrue(user.check_password('password'))
       
   def test_update_user(self):
-      user = User.objects.create_user(username='testuser',password="password", first_name="Test")   
-      self.assertEqual(user.username, 'testuser')
+      user = User.objects.create_user(user_name='testuser',password="password", first_name="Test", email="testuser@example.com")   
+      self.assertEqual(user.user_name, 'testuser')
       self.assertTrue(user.check_password('password'))
       self.assertEqual(user.first_name, 'Test')
       
-      user.username = 'updateduser'
+      user.user_name = 'updateduser'
       user.set_password('newpassword')
       user.save()
-      self.assertEqual(user.username, 'updateduser')
+      self.assertEqual(user.user_name, 'updateduser')
       self.assertTrue(user.check_password('newpassword'))
       
       
